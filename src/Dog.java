041e09ef350dfde32c0090e3ebafcdcc975a67f9
smokysse <smoky.sse@gmail.com>
2020-04-10 00:04:20 +1000

    Launcher.java Dog.java modif master

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,8 @@
-class Dog{}
-public class DogTestDrive{
+class Dog{
+  String name;
+}
+class DogTestDrive{
   public static void main(String[] args){
-  
+      Dog d1 = new Dog();
   }
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,6 @@
 public class Launcher {
     public static void main(String[] args){
-
+        Dog d1 = new Dog();
+        d1.name = "bash";
     }
 }
